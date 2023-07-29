bdf0b198edbe48ed0175ed451a781a0ec591124a
muhammmadarab <muhammadarab781@gmail.com>
2023-07-29 14:10:28 +0530

    removed strict mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-// App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-// import CreatePost from './CreatePost';
-// import UpdatePost from './UpdatePost';
-// import DeletePost from './DeletePost';
-import GetAllPosts from './pages/PostWall';
+import Post from './pages/Post';
+import UpdatePost from './pages/UpdatePost';
+import PostWall from './pages/PostWall';
 import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
@@ -16,11 +13,9 @@ const App = () => {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
-        {/* <PrivateRoute exact path="/create-post" component={CreatePost} /> */}
-        {/* <PrivateRoute exact path="/update-post/:id" component={UpdatePost} />
-        <PrivateRoute exact path="/delete-post/:id" component={DeletePost} />
-        <PrivateRoute exact path="/get-all-posts" component={GetAllPosts} /> */}
-        <PrivateRoute exact path="/" component={GetAllPosts} />
+        <PrivateRoute exact path="/update" component={UpdatePost} />
+        <PrivateRoute exact path="/create-post" component={Post} />
+        <PrivateRoute exact path="/" component={PostWall} />
         <Redirect to="/login" />
       </Switch>
     </Router>
